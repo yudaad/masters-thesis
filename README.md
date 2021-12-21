433b788c95f5c43f1853186a5afa18aa97b26faf
Yuda Andika Darmawan <yudaandikadarmawan@gmail.com>
2021-12-21 18:32:00 +0700

    New

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # masters-thesis
-This is my masters thesis. I examine the relationship between migration experience to big cities on income, both formal and informal workers based on Indonesia Family Life Survey (IFLS) dataset. I am inspired by Roca &amp; Puga (Restud, 2017).
+This is my masters thesis. I examine the relationship between migration experience to big cities on income, both formal and informal workers based on Indonesia Family Life Survey (IFLS) dataset. I am inspired by Roca &amp; Puga (Restud, 2017). IFLS is a household survey conducted by RAND Corporation, United States. Currently, IFLS has been done in 5 waves of survey. IFLS 2014 (wave 5) is currently the latest survey.   
+
+In this research, I explore the role of migration experience to Jabodetabek and other metropolitan areas on workers' income. The hypothesis here is that big cities offer many skilled workers, so that they will interact and sharing knowledge with each other in that it will accelerate human capital accumulation and learning process. Once workers leaving those big cities, they will become the highest income earner in smaller cities because of skill they have already gained in big cities. The result of this research, contradicting with my hypothesis, suggest that workers with experience in Jabodetabek doesn't have higher income. This is because workers who have experience migrating to Jabodetabek mainly come from long-distance city, such that migration cost is higher. Also, Jabodetabek, with 10+ millions citizens, creates what economists called by '*demons of density*', inequality, informality, congestion, poverty, urban polarization, and high cost of living. These *demons*, in turn, create concept '*survival of the fittest*', those who are able to deal with such density cost, will survive and be benefited from agglomeration economies, both formal and informal workers. Meanwhile, unlike workers with Jabodetabek experience, workers with migration experience to metropolitan areas receive higher income. This is because they come from near-distance areas/cities from metropolitas areas. Hence, migration cost is lower. For informal workers, near-distance means they can build network externalities to develop their businesses.  
+
+Next, I will explain what variables are used in this research:  
+
+* Dependen Variable: `Log Income`  
+Obtained from book `b3a_tk2`, and calculated by summing up 4 components:  
+  + `tk25a1` : Wage from main job
+  + `tl26a1` : Profit from main job (self-employed)
+  + `tk25b1` : Wage from side job
+  + `tk26b1` : Profit from side job
+
+* Main Variable Independent:
+  + Jabodetabek Migration Experience (Dummy variable, taken from book `b3a_mg2`)
+    - `1` : Has experience migrating to Jabodetabek for non-Jabodetabek and non-metropolitan worker
+    - `0` : Doesn't have experience migrating to Jabodetabek for non-Jabodetabek and non-metropolitan worker
+  + Metropolitan Migration Experience (Dummy taken from book `b3a_mg2`)
+    - `1` : Has experience migrating to metropolitan for non-Jabodetabek and non-metropolitan worker
+    - `0` : Doesn't have experience migrating to metropolitan for non-Jabodetabek and non-metropolitan worker
+  + Jabodetabek (Currently living when the survey conducted, taken from book `bk_sc1`)
+    - `1` : Currently live in Jabodetabek (in the time survey conducted)
+    - `0` : Currently doesn't live in Jabodetabek (in the time survey conducted)
+  + Metropolitan (Currently living when the survey conducted, taken from book `bk_sc1`)  
+    - `1` : Currently live in metropolitan (in the time survey conducted)
+    - `0` : Currently doesn't live in metropolitan (in the time survey conducted)
+
+* Variabel Independent (Control):
+  + Formal/Informal Worker (Dummy variable, taken from book `b3a_tk2` variable `tk24a`)
+    - `1` : Formal worker (private worker, government worker, and self-employed with permanent worker)
+    - `0` : Informal worker (self-employed with family worker, self-employed with temporary worker, unpaid family worker, casual worker in agriculture, casual worker in non-agriculture)
+  + Work sector (Factor variable, 9 sectors. Taken from book `b3a_tk2` variable `tk19ab`)
+  + Tenure (Integer, How many years one has been working in the current job. Taken from book `b3a_tk2` variable `tk23a2y`)
+  + Years of Schooling (Integer, years spent on school. Taken from book `b3a_dl2`)
+  + Household Head (Dummy variable, is one a household head. Taken from book `bk_ar1`)
+    - `1` : Household head
+    - `0` : Not a household head
+  + Household Size (Integer, how many family member in the household. Taken from book `bk_ar1` variable `ar02b`)
+  + Gender (Dummy variable, taken from book `bk_ar1` variable `ar07`)
+    - `1` : Male
+    - `0` : Female
+  + Age (Integer, taken from book `bk_ar1` variable `ar09`)
+  + Marital status (Dummy variable, taken from book `bk_ar1` variable `ar13`)
+  + Gross Domestic Regional Bruto (Numeric, taken from Simreg Bappenas BPS)
+  + Minimum Wage (Numeric, taken from BPS)
+
+
+
+
--- script_thesis.R
+++ script_thesis.R
@@ -2,19 +2,68 @@ library(dplyr)
 library(haven)
 library(expss)
 library(stringr)
-library(tidyverse)
-
-# Set Working Directory
-setwd("D:/R/thesis")
+library(tidyr)
+library(readxl)
+library(labelled)
+library(ggplot2)
+library(MetBrewer)
+library(modelsummary)
+library(stargazer)
 
 # Import Data----
-b3a_mg1 <- read_dta('b3a_mg1.dta')
-b3a_mg2 <- read_dta('b3a_mg2.dta')
-b3a_tk1 <- read_dta('b3a_tk1.dta')
-b3a_tk2 <- read_dta('b3a_tk2.dta')
-bk_ar1 <- read_dta('bk_ar1.dta')
-b3a_dl1 <- read_dta('b3a_dl1.dta')
+bk_sc1 <- read_dta('bk_sc1.dta') # Current residency data
+b3a_mg1 <- read_dta('b3a_mg1.dta') # migration data
+b3a_mg2 <- read_dta('b3a_mg2.dta') # migration destination data
+b3a_tk1 <- read_dta('b3a_tk1.dta') # labor characteristics data
+b3a_tk2 <- read_dta('b3a_tk2.dta') # labor characteristics data
+bk_ar1 <- read_dta('bk_ar1.dta') # individual characteristics data
+b3a_dl1 <- read_dta('b3a_dl1.dta') # individual education data
+umr_14 <- read_excel('umr_prov_2014.xlsx')
+gdrp_14 <- read_excel('gdrp14.xlsx')
 
+#==============================================================================
+# Working with bk_sc1 data (current residency data)----
+# Create 4-digits BPS city code and dummy variable jabodetabek and metro
+bk_sc1 <- bk_sc1 %>%
+  mutate_at(c('sc01_14_14', 'sc02_14_14'), as.character) %>%
+  mutate(kodekab = ifelse(str_length(sc02_14_14) == 1
+                          , paste0("0", sc02_14_14), sc02_14_14),
+         kabcode14 = paste0(sc01_14_14, kodekab),
+         jabodetabek = ifelse(kabcode14 %in% c(3171, 3172, 3173, 3174, 3175,
+                       3271, 3276, 3275, 3671, 3674, 3201, 3216, 3603), 1, 0),
+         metro = ifelse(kabcode14 %in% c(3578, 1275, 3273, 7371, 3374, 1671, 
+                                         1871, 2171), 1, 0))
+
+# recode urban/rural variable to dummy variable
+bk_sc1$sc05[bk_sc1$sc05 == 2] <- 0
+
+# Finishing
+# Select Data
+city <- bk_sc1 %>%
+  rename(urban = sc05,
+         provcode14 = sc01_14_14) %>%
+  select(hhid14, provcode14, kabcode14, urban, jabodetabek, metro)
+
+# change data type
+city$provcode14 <- as.character(city$provcode14)
+city$kabcode14 <- as.character(city$kabcode14)
+city$urban <- as.factor(city$urban)
+city$jabodetabek <- as.factor(city$jabodetabek)
+city$metro <- as.factor(city$metro)
+
+# remove value labels
+city$urban <- remove_val_labels(city$urban)
+
+# Apply data labels
+city <- apply_labels(city,
+                     urban = "Urban",
+                     provcode14 = "BPS Province Code",
+                     kabcode14 = "BPS City Code",
+                     jabodetabek = "Jabodetabek",
+                     metro = "Metropolitan")
+
+
+#==============================================================================
 # Working with b3a_tk2 data (Working Characteristics Data)----
 
 # Generate income variable
@@ -47,6 +96,14 @@ work <- b3a_tk2 %>%
          ln_income = log(income)) %>%
   select(hhid14, pid14, pidlink, income, ln_income, formal, sector, tenure, tenure2)
 
+# Change data type
+work$pid14 <- as.character(work$pid14)
+work$formal <- as.factor(work$formal)
+work$sector <- as.factor(work$sector)
+
+varlist <- c(4,5,8,9)
+work[varlist] <- sapply(work[varlist], as.numeric)
+
 # Apply data label
 work <- apply_labels(work,
                      sector = "Working Sector",
@@ -56,6 +113,7 @@ work <- apply_labels(work,
                      tenure = "Tenure",
                      tenure2 = "Tenure Squared")
 
+#==============================================================================
 # Working with bk_ar1 data (Individual Characteristics Data)----
 
 # Age variables (ar09)
@@ -78,6 +136,9 @@ bk_ar1$ar13[bk_ar1$ar13 == 2] <- 1
 
 # Transform into factor
 bk_ar1$ar07 <- as.factor(bk_ar1$ar07)
+
+# remove value label
+bk_ar1$ar07 <- remove_val_labels(bk_ar1$ar07)
 
 # Household head variables (ar02b)
 bk_ar1 <- bk_ar1 %>%
@@ -109,6 +170,13 @@ ch <- bk_ar1 %>%
   filter(ar01a %in% c(1, 2, 5, 11)) %>%
   select(hhid14, pid14, pidlink, age, male, married, hh_head, hh_size, ethnic)
 
+# change data type
+ch$pid14 <- as.character(ch$pid14)
+ch$age <- as.integer(ch$age)
+ch$married <- as.factor(ch$married)
+ch$hh_size <- as.integer(ch$hh_size)
+
+# Apply label variable
 ch <- apply_labels(ch,
                    age = "Age",
                    male = "Male",
@@ -117,6 +185,7 @@ ch <- apply_labels(ch,
                    hh_size = "Household Size",
                    ethnic = "Ethnicity")
 
+#==============================================================================
 # Working with b3a_dl1 data (Schooling Data)----
 
 # Create years of schooling variable
@@ -249,10 +318,15 @@ b3a_dl1 <- b3a_dl1 %>%
 yos <- b3a_dl1 %>%
   select(hhid14, pid14, pidlink, schooling)
 
+# change data type
+yos$schooling <- as.integer(yos$schooling)
+yos$pid14 <- as.character(yos$pid14)
+
+# labeling
 yos <- apply_labels(yos,
                     schooling = "Years of Schooling")
 
-
+#==============================================================================
 # Working with Migration Data----
 
 # Creating 4-digits BPS city code, and migration experience to Jabodetabek and metropolitan
@@ -277,3 +351,168 @@ b3a_mg2 <- b3a_mg2 %>%
 # Replacing back missing value to zero
 b3a_mg2$jabodetabek_exp[is.na(b3a_mg2$jabodetabek_exp)] <- 0
 b3a_mg2$metro_exp[is.na(b3a_mg2$metro_exp)] <- 0
+
+# Finishing
+# select variables
+mig <- b3a_mg2 %>%
+  rename(provcode14_mig = mg21d,
+         kabcode14_mig = kabcode14) %>%
+  select(hhid14, pid14, pidlink, provcode14_mig, kabcode14_mig, jabodetabek_exp, metro_exp)
+
+# Remove duplicates
+mig <- distinct(mig, pidlink, .keep_all = TRUE)
+
+# Eliminate those who are currently living in Jabodetabek and metropolitan area
+# merge with city currently living
+mig <- inner_join(city, mig, by = 'hhid14')
+
+# recode value jabodetabek_exp/metro_exp to 0 if one's currently living in jabodetabek/metro
+mig$jabodetabek_exp[mig$jabodetabek == 1] <- 0
+mig$metro_exp[mig$metro == 1] <- 0
+
+# Select data
+mig <- mig %>%
+  select(hhid14, pid14, pidlink, jabodetabek_exp, metro_exp)
+
+# change data types
+mig$jabodetabek_exp <- as.factor(mig$jabodetabek_exp)
+mig$metro_exp <- as.factor(mig$metro_exp)
+mig$pid14 <- as.character(mig$pid14)
+
+# Insert label
+mig <- apply_labels(mig,
+                    jabodetabek_exp = "Jabodetabek Experience",
+                    metro_exp = "Metropolitan Experience")
+
+#==============================================================================
+# Working with macro data ----
+# cleaning up gdrp data
+gdrp_14 <- gdrp_14 %>%
+  mutate_at('kodekab14', as.character) %>%
+  rename(kabcode14 = kodekab14,
+         regency = nama_wilayah) %>%
+  mutate(ln_gdrp14 = log(gdrp14)) %>%
+  select(kabcode14, regency, gdrp14, ln_gdrp14)
+
+gdrp_14 <- apply_labels(gdrp_14,
+                        kabcode14 = "BPS City Code",
+                        regency = "Regency/City Name",
+                        gdrp14 = 'GDRP',
+                        ln_gdrp14 = "Log GDRP")
+
+# cleaning up minimum wage data
+umr_14 <- umr_14 %>%
+  mutate_at('kodeprov14', as.character) %>%
+  rename(provcode14 = kodeprov14,
+         province = provinsi,
+         mw14 = umr14) %>%
+  mutate(ln_mw14 = log(mw14)) %>%
+  select(provcode14, province, mw14, ln_mw14)
+
+umr_14 <- apply_labels(umr_14,
+                       provcode14 = "BPS Province Code",
+                       province = "Province",
+                       mw14 = "Minimum Wage",
+                       ln_mw14 = "Log Minimum Wage")
+  
+
+#==============================================================================
+# Merging data----
+# Merging individual characteristics and working data
+compiled <- inner_join(ch, work, by = c('hhid14', 'pid14', 'pidlink'))
+
+# merging compiled data with city currently living data
+compiled <- inner_join(compiled, city, by = 'hhid14')
+
+# merging with GDRP data
+compiled <- inner_join(compiled, gdrp_14, by = 'kabcode14')
+
+# merging with minimum wage data
+compiled <- inner_join(compiled, umr_14, by = 'provcode14')
+
+# merging with schooling data
+compiled <- inner_join(compiled, yos, by = c('hhid14', 'pid14', 'pidlink'))
+
+# merging with migration experience data
+compiled <- left_join(compiled, mig, by = c('hhid14', 'pid14', 'pidlink'))
+
+# assign zero value to NA in variable jabodetabek_exp and metro_exp
+compiled$jabodetabek_exp[is.na(compiled$jabodetabek_exp)] <- 0
+compiled$metro_exp[is.na(compiled$metro_exp)] <- 0
+
+#==============================================================================
+# Plotting Data ----
+
+# Plot metropolitan experience and mean income, grouped by job informality
+ggplot(data = compiled, aes(x = metro_exp, y = income, fill = formal)) +
+  stat_summary(fun = "mean", geom = 'bar', position = 'dodge') +
+  stat_summary(aes(label=round(..y..,2)), fun=mean, geom="text", size=3.5,
+               vjust = 5, position = position_dodge(width = 0.9), color = 'black') +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = position_dodge(width = 0.9), 
+               width=0.4, color = 'black') +
+  scale_fill_manual(values=met.brewer("Isfahan2", n = 2)) +
+  theme_classic()
+
+# Plot jabodetabek experience and mean income, grouped by job informality
+ggplot(data = compiled, aes(x = jabodetabek_exp, y = income, fill = formal)) +
+  stat_summary(fun = "mean", geom = 'bar', position = 'dodge') +
+  stat_summary(aes(label=round(..y..,2)), fun=mean, geom="text", size=3.5,
+               vjust = 5, position = position_dodge(width = 0.9), color = 'black') +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = position_dodge(width = 0.9), 
+               width=0.4, color = 'black') +
+  scale_fill_manual(values=met.brewer("Isfahan2", n = 2)) +
+  theme_classic()
+
+# Plot living in Jabodetabek and mean income, grouped by job informality
+ggplot(data = compiled, aes(x = jabodetabek, y = income, fill = formal)) +
+  stat_summary(fun = "mean", geom = 'bar', position = 'dodge') +
+  stat_summary(aes(label=round(..y..,2)), fun=mean, geom="text", size=3.5,
+               vjust = 5, position = position_dodge(width = 0.9), color = 'black') +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = position_dodge(width = 0.9), 
+               width=0.4, color = 'black') +
+  scale_fill_manual(values=met.brewer("Isfahan2", n = 2)) +
+  theme_classic()
+
+# Plot living in metropolitan and mean income, grouped by job informality
+ggplot(data = compiled, aes(x = metro, y = income, fill = formal)) +
+  stat_summary(fun = "mean", geom = 'bar', position = 'dodge') +
+  stat_summary(aes(label=round(..y..,2)), fun=mean, geom="text", size=3.5,
+               vjust = 5, position = position_dodge(width = 0.9), color = 'black') +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = position_dodge(width = 0.9), 
+               width=0.4, color = 'black') +
+  scale_fill_manual(values=met.brewer("Isfahan2", n = 2)) +
+  theme_classic()
+
+#==============================================================================
+# Regression ----
+ols1 <- lm(ln_income ~ jabodetabek_exp + metro_exp + jabodetabek + metro + 
+             tenure + tenure2 + formal + sector + schooling + age + 
+             male + married + hh_head + hh_size + ln_gdrp14 + ln_mw14
+             , data = compiled)
+
+
+ols_informal <- lm(ln_income ~ jabodetabek_exp + metro_exp + jabodetabek + metro + 
+                   tenure + tenure2 + sector + schooling + age + 
+                   male + married + hh_head + hh_size + ln_gdrp14 + ln_mw14 
+                   , data = compiled %>% filter(formal == 0))
+
+
+ols_formal <- lm(ln_income ~ jabodetabek_exp + metro_exp + jabodetabek + metro + 
+                     tenure + tenure2 + sector + schooling + age + 
+                     male + married + hh_head + hh_size + ln_gdrp14 + ln_mw14
+                     , data = compiled %>% filter(formal == 1))
+
+stargazer(ols1, ols_informal, ols_formal
+          , type = 'html'
+          , out = 'regression_output.html')
+
+
+
+
+
+
+
+
+
+
+
